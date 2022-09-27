2671f954bfbf6ed447134b6825b4be52125a077c
joel-mal23 <joel.malvaez.23@gmail.com>
2022-09-27 09:18:25 -0500

    repo ms

---
+++ Modulo-Clientes/src/main/java/MicroClientes/ModuloClientes/ModuloClientesApplication.java
@@ -0,0 +1,13 @@
+package MicroClientes.ModuloClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloClientesApplication.class, args);
+	}
+
+}
